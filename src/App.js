dc07026b90eded866dd83f7766bc21e71b4625af
andrewtanjaya <andrewtanjaya21@gmail.com>
2021-07-14 17:59:15 +0700

    Mapping through data to card list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component{
   constructor(){
@@ -17,11 +18,14 @@ class App extends Component{
   render(){
     return (
       <div className="App">
+        <CardList name = "andrew">
         {
           this.state.monsters.map( monster =>
             <h1 key={monster.id}>{monster.name}</h1>
           )
         }
+        </CardList>
+        
       </div>
     );
   }
