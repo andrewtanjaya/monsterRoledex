cf6ce17f479397a8f2aa6856e1f877bd8bac1c1c
andrewtanjaya <andrewtanjaya21@gmail.com>
2021-07-14 18:15:27 +0700

    Added new component + robohash pic

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './card-list.style.css';
+import {Card} from '../card/card.component'
 
 export const CardList= (props) =>{
     console.log(props)
-    return <div className="card-list">{props.children}</div>
+    return <div className="card-list">{
+       props.monsters.map( monster =>
+          <Card key={monster.id} monster = {monster}></Card>
+        )
+      }</div>
 }
